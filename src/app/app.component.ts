78536d6ac197ef855ce438c2efecea9096262ccf
Abner Aragon - 95620 <em.mansi@outlook.com>
2024-06-08 17:34:03 -0300

    Fazendo o programa rodar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,34 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { FiltersComponent } from "./components/filters/filters.component";
 import { DataTableComponent } from "./components/data-table/data-table.component";
 import { BarraInicialComponent } from './components/barra-inicial/barra-inicial.component';
-import { OceanDataService } from './services/ocean-data.service';
-import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
-
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FiltersComponent, DataTableComponent, BarraInicialComponent, OceanDataService],
+  imports: [RouterOutlet, FiltersComponent, DataTableComponent, BarraInicialComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
-
 })
-
 export class AppComponent {
-
   title = 'Oceanic Data Explorer';
-
-  constructor(private http: HttpClient) {
-    // Configuração do HttpClientModule
-    provideHttpClient.forRoot({
-      provide: HttpClient,
-      useFactory: provideHttpClient,
-      deps: []
-    });
-  }
-
-}
-
-export function provideHttpClient() {
-  return new HttpClient();
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { routes } from './app/app.routes';
+import { provideRouter } from '@angular/router';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withFetch())
+  ]
+}).catch(err => console.error(err));
